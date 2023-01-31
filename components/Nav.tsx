34744872c93a0dfb71a876ab8804c94295296e12
Thomas Broadwater <tgbroadh2o@gmail.com>
2023-01-31 14:49:43 -0500

    Basic layout

--- components/Nav.tsx
+++ components/Nav.tsx
@@ -9,15 +9,24 @@ export default function Nav() {
 
   if (!focus) return (
     <>
-      <AnimatePresence>
+      <AnimatePresence initial={false}>
         <div className={styles.bar} key='h-icon'>
+          {/* TODO: SVG Path animation */}
           <motion.img
             className={styles.icon}
             src='/hamburger.svg'
             onClick={() => setFocus(true)}
             initial={{ opacity: 0}}
-            animate={{ opacity: 1 }}
+            animate={{
+              opacity: 1,
+              transition: {
+                delay: 0.25
+              }
+            }}
             exit={{ opacity: 0 }}
+            transition={{
+              duration: 0.25
+            }}
           />
         </div>
       </AnimatePresence>
@@ -28,23 +37,39 @@ export default function Nav() {
     <>
       <AnimatePresence>
         <div className={styles.bar} key='x-icon'>
+          {/* TODO: SVG Path animation */}
           <motion.img
             className={styles.icon}
             src='/xmark.svg'
             onClick={() => setFocus(false)}
             initial={{ opacity: 0}}
-            animate={{ opacity: 1 }}
+            animate={{
+              opacity: 1,
+              transition: {
+                delay: 0.25
+              }
+            }}
             exit={{ opacity: 0 }}
+            transition={{
+              duration: 0.25
+            }}
           />
         </div>
+        {/* TODO: animation delay timing */}
         <motion.div
           className={styles.container}
           key='menu'
           initial={{ x: '100vw' }}
           animate={{ x: 0 }}
           exit={{ x: '-100vw' }}
+          transition={{
+            duration: 0.7,
+            ease: 'easeOut'
+          }}
         >
-          <h1>TEST</h1>
+          <p>about</p>
+          <p>blog</p>
+          <p>contact</p>
         </motion.div>
       </AnimatePresence>
     </>
